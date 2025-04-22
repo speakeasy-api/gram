a504fe0e2948bc1dc0ec0874ae12a16aeb7fa91e
Ryan Albert <ryantimalbert@gmail.com>
2025-04-22 12:54:42 -0700

    feat: add openai agents sdk example

---
+++ sdks/python/src/examples/openai_agents.py
@@ -0,0 +1,33 @@
+import asyncio
+import os
+from agents import Agent, Runner, set_default_openai_key
+from gram_ai.openai_agents import GramOpenAIAgents
+
+key = os.getenv("GRAM_API_KEY")
+
+gram = GramOpenAIAgents(
+    api_key=key,
+)
+
+set_default_openai_key(os.getenv("GRAM_OPENAI_API_KEY"))
+
+agent = Agent(
+    name="Assistant",
+    tools=gram.tools(
+        project="default",
+        toolset="test",
+        environment="default",
+    ),
+)
+
+
+async def main():
+    result = await Runner.run(
+        agent,
+        "Can you get me the speakeasy organization ryan-local.",
+    )
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main()) 
