e62035abfee83ccaa78aad3d6a66a720f080f9f0
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2025-04-24 10:25:19 -0700

    feat: vanilla function calling client python (#61)

---
+++ sdks/python/src/gram_ai/functioncalling.py
@@ -0,0 +1,159 @@
+from dataclasses import dataclass
+import functools
+import json
+from typing import Callable, Dict, Optional, Awaitable, Union
+
+import httpx
+
+from gram_ai import VERSION, GramAPI
+from gram_ai.models.getinstanceresult import GetInstanceResult
+from gram_ai.utils.retries import BackoffStrategy, Retries, RetryConfig, retry_async, retry
+
+
+@dataclass
+class GramFunctionCallingCall:
+    tool_id: str
+    project: str
+    toolset: str
+    environment: Optional[str] = None
+
+
+@dataclass
+class GramFunctionCallingTool:
+    name: str
+    description: str
+    parameters: Dict
+    execute: Callable[[str], str]
+    aexecute: Callable[[str], Awaitable[str]]
+
+
+class GramFunctionCalling:
+    api_key: str
+    _cache: dict[tuple[str, str, Union[str, None]], list[GramFunctionCallingTool]] = {}
+
+    def __init__(
+        self,
+        *,
+        api_key: str,
+    ):
+        self.api_key = api_key
+        self.client = GramAPI(server_url="http://localhost:8080")
+
+    def _fetch_tools(
+        self,
+        project: str,
+        toolset: str,
+        environment: Optional[str] = None,
+    ) -> GetInstanceResult:
+        return self.client.instances.get_by_slug(
+            security={
+                "option2": {
+                    "apikey_header_gram_key": self.api_key,
+                    "project_slug_header_gram_project": project,
+                }
+            },
+            toolset_slug=toolset,
+            environment_slug=environment,
+        )
+
+    def tools(
+        self,
+        project: str,
+        toolset: str,
+        environment: Optional[str] = None,
+    ) -> list[GramFunctionCallingTool]:
+        key = (project, toolset, environment)
+        if key in self._cache:
+            return self._cache[key]
+
+        instance = self._fetch_tools(project, toolset, environment)
+
+        tools: list[GramFunctionCallingTool] = []
+
+        for tool in instance.tools:
+            schema = json.loads(tool.schema_) if tool.schema_ else {}
+
+            tool_call = GramFunctionCallingCall(tool.id, project, toolset, environment)
+
+            func = self._create_sync_tool_function(tool_call)
+            async_func = self._create_tool_function(tool_call)
+
+            tools.append(GramFunctionCallingTool(
+                name=tool.name,
+                description=tool.description,
+                parameters=schema,
+                execute=func,
+                aexecute=async_func
+            ))
+
+        self._cache[key] = tools
+        return tools
+
+    def _prepare_request(self, tool_call: GramFunctionCallingCall, input: str):
+        url = "http://localhost:8080/rpc/instances.invoke/tool"
+        params = {"tool_id": tool_call.tool_id}
+        if tool_call.environment:
+            params["environment_slug"] = tool_call.environment
+        
+        json_input = json.loads(input)
+
+        return url, params, {
+            "gram-key": self.api_key,
+            "gram-project": tool_call.project,
+            "user-agent": f"gram-ai/openai-agents python {VERSION}",
+            "content-type": "application/json",
+        }, json_input
+
+    def _create_tool_function(self, tool_call: GramFunctionCallingCall):
+        async def wrapper(input: str):
+            url, params, headers, data = self._prepare_request(tool_call, input)
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    url=url,
+                    params=params,
+                    headers=headers,
+                    json=data,
+                )
+                response = await retry_async(
+                    functools.partial(client.send, response.request),
+                    _retry_policy
+                )
+                response.raise_for_status()
+                return response.text
+        return wrapper
+
+    def _create_sync_tool_function(self, tool_call: GramFunctionCallingCall):
+        def wrapper(input: str):
+            url, params, headers, data = self._prepare_request(tool_call, input)
+            with httpx.Client() as client:
+                response = client.post(
+                    url=url,
+                    params=params,
+                    headers=headers,
+                    json=data,
+                )
+                response = retry(
+                    functools.partial(client.send, response.request),
+                    _retry_policy
+                )
+                response.raise_for_status()
+                return response.text
+        return wrapper
+
+
+_retry_policy = Retries(
+    config=RetryConfig(
+        strategy="backoff",
+        backoff=BackoffStrategy(
+            initial_interval=500,
+            max_interval=60000,
+            exponent=1.5,
+            max_elapsed_time=3600000,
+        ),
+        retry_connection_errors=True,
+    ),
+    status_codes=["429", "5XX"],
+)
+
+
+__all__ = ["GramFunctionCalling"]
