cf21796098fe3ea0e6cb1f1406db31177caca974
Georges Haidar <ghaidar0@gmail.com>
2025-04-25 11:35:13 +0000

    chore: allow vite dev server to be accessed through wsl/vms/containers

--- client/dashboard/vite.config.ts
+++ client/dashboard/vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: ["localhost", "127.0.0.1", "devbox"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
