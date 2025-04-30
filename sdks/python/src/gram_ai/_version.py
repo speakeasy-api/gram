d1415d7f3f78d95a6c0351afcddefa5c91a9ca31
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2025-04-29 17:27:19 -0700

    feat: update (#108)

--- sdks/python/src/gram_ai/_version.py
+++ sdks/python/src/gram_ai/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "gram-ai"
-__version__: str = "0.2.1"
+__version__: str = "0.0.1"
 __openapi_doc_version__: str = "0.0.1"
-__gen_version__: str = "2.588.4"
-__user_agent__: str = "speakeasy-sdk/python 0.2.1 2.588.4 0.0.1 gram-ai"
+__gen_version__: str = "2.593.4"
+__user_agent__: str = "speakeasy-sdk/python 0.0.1 2.593.4 0.0.1 gram-ai"
 
 try:
     if __package__ is not None:
