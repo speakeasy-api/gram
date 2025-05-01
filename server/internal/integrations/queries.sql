85391a02d1f6f842aa6ae520b258088cd904212d
Georges Haidar <ghaidar0@gmail.com>
2025-05-01 11:14:17 +0000

    chore: switch to lateral join for top-1 query on packages

--- server/internal/integrations/queries.sql
+++ server/internal/integrations/queries.sql
@@ -1,24 +1,4 @@
 -- name: ListIntegrations :many
-WITH latest_versions AS (
-    -- Select package versions that are not prereleases versions,
-    -- and assign a rank within each package based on version number (highest first).
-    SELECT
-        pv.package_id,
-        pv.deployment_id,
-        pv.major,
-        pv.minor,
-        pv.patch,
-        pv.prerelease,
-        pv.build,
-        pv.created_at,
-        ROW_NUMBER() OVER(PARTITION BY pv.package_id ORDER BY pv.major DESC, pv.minor DESC, pv.patch DESC) as rn
-    FROM
-        package_versions pv
-    WHERE
-        pv.visibility = 'public'
-        AND pv.prerelease IS NULL -- Exclude prerelease versions
-        AND pv.deleted IS FALSE  -- Exclude soft-deleted versions
-)
 SELECT 
   p.id AS package_id,
   p.name AS package_name,
@@ -31,7 +11,23 @@ SELECT
   lv.prerelease AS version_prerelease,
   lv.build AS version_build,
   lv.created_at AS version_created_at,
-  (SELECT COUNT(*) FROM http_tool_definitions WHERE http_tool_definitions.deployment_id = lv.deployment_id) as tool_count
+  (SELECT COUNT(id) FROM http_tool_definitions WHERE http_tool_definitions.deployment_id = lv.deployment_id) as tool_count
 FROM packages p
-JOIN latest_versions lv ON p.id = lv.package_id
+JOIN LATERAL (
+    SELECT
+        pv.deployment_id,
+        pv.major,
+        pv.minor,
+        pv.patch,
+        pv.prerelease,
+        pv.build,
+        pv.created_at
+    FROM package_versions pv
+    WHERE pv.package_id = p.id
+        AND pv.visibility = 'public'
+        AND pv.prerelease IS NULL -- Exclude prerelease versions
+        AND pv.deleted IS FALSE  -- Exclude soft-deleted versions
+    ORDER BY pv.major DESC, pv.minor DESC, pv.patch DESC
+    LIMIT 1
+) lv ON TRUE
 WHERE p.deleted IS FALSE;
