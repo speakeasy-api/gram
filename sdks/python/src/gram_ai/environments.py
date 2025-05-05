fd1ba7a5f72bda6d67b388cd3b862042f9d8b08b
Ryan Albert <ryantimalbert@gmail.com>
2025-05-05 13:50:27 -0700

    feat: updates

--- sdks/python/src/gram_ai/environments.py
+++ sdks/python/src/gram_ai/environments.py
@@ -2,7 +2,7 @@ def get_server_url_by_key(key: str) -> str:
     if key.startswith("gram_local_"):
         return "http://localhost:8080"
     if key.startswith("gram_test_"):
-        return "https://dev.getgram.ai"
+        return "http://dev.getgram.ai"
     if key.startswith("gram_live_"):
         return "https://getgram.ai"
     return "https://getgram.ai"
