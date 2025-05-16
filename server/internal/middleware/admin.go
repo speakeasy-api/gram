27a0586b5aded073d07a5ad603907a58aa0de7c0
Georges Haidar <ghaidar0@gmail.com>
2025-05-16 16:31:25 +0100

    chore: support gram_admin_override cookie (#195)
    This change allows the cookie to gram_admin_override to change the
    organization that the user is authenticated as in addition to the
    header.

--- server/internal/middleware/admin.go
+++ server/internal/middleware/admin.go
@@ -8,7 +8,14 @@ import (
 
 func AdminOverrideMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if value := r.Header.Get("Gram-Admin-Override"); value != "" {
+		value := r.Header.Get("Gram-Admin-Override")
+		if value == "" {
+			if c, err := r.Cookie("gram_admin_override"); err == nil {
+				value = c.Value
+			}
+		}
+
+		if value != "" {
 			ctx := contextvalues.SetAdminOverrideInContext(r.Context(), value)
 			r = r.WithContext(ctx)
 		}
