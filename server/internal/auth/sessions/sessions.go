7d596d750f975e9dfeeb2903c6431bcca6d49acd
Ryan Albert <ryantimalbert@gmail.com>
2025-06-18 17:07:05 -0700

    fix: deploy santity check for org metadata

--- server/internal/auth/sessions/sessions.go
+++ server/internal/auth/sessions/sessions.go
@@ -85,13 +85,23 @@ func (s *Manager) Authenticate(ctx context.Context, key string, canStubAuth bool
 		ProjectSlug:          nil,
 	}
 
-	if _, ok := s.HasAccessToOrganization(ctx, session.ActiveOrganizationID, session.UserID, session.SessionID); !ok {
+	org, ok := s.HasAccessToOrganization(ctx, session.ActiveOrganizationID, session.UserID, session.SessionID)
+	if !ok {
 		return ctx, oops.C(oops.CodeForbidden)
 	}
 
-	orgMetadata, err := s.orgRepo.GetOrganizationMetadata(ctx, session.ActiveOrganizationID)
+	var orgMetadata orgRepo.OrganizationMetadatum
+	orgMetadata, err = s.orgRepo.GetOrganizationMetadata(ctx, session.ActiveOrganizationID)
 	if err != nil {
-		return ctx, oops.E(oops.CodeUnexpected, err, "error loading organization metadata").Log(ctx, s.logger)
+		// TODO: Temporary measure while we are backfilling org metadata
+		orgMetadata, err = s.orgRepo.UpsertOrganizationMetadata(ctx, orgRepo.UpsertOrganizationMetadataParams{
+			ID:   session.ActiveOrganizationID,
+			Name: org.Name,
+			Slug: org.Slug,
+		})
+		if err != nil {
+			return ctx, oops.E(oops.CodeUnexpected, err, "error getting organization metadata").Log(ctx, s.logger)
+		}
 	}
 	authCtx.AccountType = orgMetadata.GramAccountType
 	authCtx.OrganizationSlug = orgMetadata.Slug
