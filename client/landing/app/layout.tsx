bc907cd82b91beaef20fc528eed1852145cf84cc
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2025-07-08 15:58:44 -0700

    feat: rb2b tracking scripts (#458)
    https://linear.app/speakeasy/issue/AGE-197/add-rb2bcom-pixel-to-getgramai

--- client/landing/app/layout.tsx
+++ client/landing/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const diatype = localFont({
@@ -81,6 +82,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="reb2b-tracking"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/1N5W0HMGDKO5.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("1N5W0HMGDKO5");}();`
+        }}
+      />
       <body
         className={`
           ${diatype.variable} 
