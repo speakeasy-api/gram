c8b8d5e74886ed58e2ec3dd9f764f3dae8ec045b
Georges Haidar <ghaidar0@gmail.com>
2025-08-14 11:05:22 +0000

    remove path prefix

--- .mise-tasks/datadog/sourcemaps.sh
+++ .mise-tasks/datadog/sourcemaps.sh
@@ -16,5 +16,4 @@ fi
 exec pnpm datadog-ci sourcemaps upload ./client/dashboard/dist \
   --service gram \
   --release-version "${usage_git_sha:?}" \
-  --minified-path-prefix /assets \
   --dry-run
