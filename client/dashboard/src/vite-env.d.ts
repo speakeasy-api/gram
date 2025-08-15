703f01915f80d573359aa3114dee3e8d84b0955e
Georges Haidar <ghaidar0@gmail.com>
2025-08-15 12:43:58 +0000

    fix: override env vars for vite production builds

--- client/dashboard/src/vite-env.d.ts
+++ client/dashboard/src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-declare const __GRAM_SERVER_URL__: string;
+declare const __GRAM_SERVER_URL__: string | undefined;
 declare const __GRAM_GIT_SHA__: string | undefined;
 
 interface ViteTypeOptions {
