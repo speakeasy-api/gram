999ef6a6f6e4e75213cda0f3eed51a012152a240
Ryan Albert <ryantimalbert@gmail.com>
2025-09-03 15:35:44 -0700

    feat: update

---
+++ server/internal/background/activities/get_all_organizations.go
@@ -0,0 +1,38 @@
+package activities
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/speakeasy-api/gram/server/internal/attr"
+	"github.com/speakeasy-api/gram/server/internal/background/activities/repo"
+	"github.com/speakeasy-api/gram/server/internal/oops"
+)
+
+type GetAllOrganizations struct {
+	logger *slog.Logger
+	repo   *repo.Queries
+}
+
+func NewGetAllOrganizations(logger *slog.Logger, db *pgxpool.Pool) *GetAllOrganizations {
+	return &GetAllOrganizations{
+		logger: logger.With(attr.SlogComponent("get-all-organizations")),
+		repo:   repo.New(db),
+	}
+}
+
+func (g *GetAllOrganizations) Do(ctx context.Context) ([]string, error) {
+	orgs, err := g.repo.GetAllOrganizations(ctx)
+	if err != nil {
+		return nil, oops.E(oops.CodeUnexpected, err, "failed to get all organization").Log(ctx, g.logger)
+	}
+
+	orgIDs := make([]string, len(orgs))
+	for i, org := range orgs {
+		orgIDs[i] = org.ID
+	}
+
+	g.logger.InfoContext(ctx, "retrieved all organization IDs successfully")
+	return orgIDs, nil
+}
