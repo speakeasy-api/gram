08d549c5601cffb458d5f3cbeebd7880c736d881
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2025-09-08 15:55:53 -0700

    feat: disable add domain button (#186)
    Disable the Add Domain button if account type is free. 
    
    The backend still would have blocked the add request, but the UX is
    weird if you can open the domain model by clicking on a different Add
    Domain button.

--- client/dashboard/src/pages/settings/Settings.tsx
+++ client/dashboard/src/pages/settings/Settings.tsx
@@ -458,8 +458,15 @@ export default function Settings() {
               <Button
                 size="sm"
                 variant="secondary"
-                onClick={() => setIsAddDomainDialogOpen(true)}
                 icon={"globe"}
+                onClick={() => {
+                  if (session.gramAccountType === "free") {
+                    setIsCustomDomainModalOpen(true);
+                  } else {
+                    setIsAddDomainDialogOpen(true);
+                  }
+                }}
+                disabled={domain?.isUpdating}
               >
                 Add Domain
               </Button>
