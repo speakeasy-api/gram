658fd030cc7af5515ba0dab051826c48be30a71d
Robert Crumbaugh <chasecrumbaugh4@gmail.com>
2025-09-08 12:42:25 -0700

    chore: Update frontend dependencies

--- client/sdk/src/lib/config.ts
+++ client/sdk/src/lib/config.ts
@@ -59,7 +59,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.0.1",
-  sdkVersion: "0.12.59",
-  genVersion: "2.687.13",
-  userAgent: "speakeasy-sdk/typescript 0.12.59 2.687.13 0.0.1 @gram/client",
+  sdkVersion: "0.12.60",
+  genVersion: "2.696.0",
+  userAgent: "speakeasy-sdk/typescript 0.12.60 2.696.0 0.0.1 @gram/client",
 } as const;
