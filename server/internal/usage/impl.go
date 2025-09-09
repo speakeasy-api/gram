ff52b0488f7c4bf2f428553c16b6f19869981bd7
Chase <chasecrumbaugh4@gmail.com>
2025-09-09 14:48:59 -0700

    chore: capture subscription changes in posthog (#201)
    Forward subscription changes to posthog for tracking purposes

--- server/internal/usage/impl.go
+++ server/internal/usage/impl.go
@@ -23,6 +23,7 @@ import (
 	"github.com/speakeasy-api/gram/server/internal/o11y"
 	"github.com/speakeasy-api/gram/server/internal/oops"
 	orgRepo "github.com/speakeasy-api/gram/server/internal/organizations/repo"
+	"github.com/speakeasy-api/gram/server/internal/thirdparty/posthog"
 	"github.com/speakeasy-api/gram/server/internal/usage/repo"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
@@ -31,28 +32,30 @@ import (
 )
 
 type Service struct {
-	tracer      trace.Tracer
-	logger      *slog.Logger
-	auth        *auth.Auth
-	serverURL   *url.URL
-	repo        *repo.Queries
-	billingRepo billing.Repository
-	orgRepo     *orgRepo.Queries
+	tracer        trace.Tracer
+	logger        *slog.Logger
+	auth          *auth.Auth
+	serverURL     *url.URL
+	repo          *repo.Queries
+	billingRepo   billing.Repository
+	orgRepo       *orgRepo.Queries
+	posthogClient *posthog.Posthog
 }
 
 var _ gen.Service = (*Service)(nil)
 
-func NewService(logger *slog.Logger, db *pgxpool.Pool, sessions *sessions.Manager, billingRepo billing.Repository, serverURL *url.URL) *Service {
+func NewService(logger *slog.Logger, db *pgxpool.Pool, sessions *sessions.Manager, billingRepo billing.Repository, serverURL *url.URL, posthogClient *posthog.Posthog) *Service {
 	logger = logger.With(attr.SlogComponent("usage"))
 
 	return &Service{
-		tracer:      otel.Tracer("github.com/speakeasy-api/gram/server/internal/usage"),
-		logger:      logger,
-		auth:        auth.New(logger, db, sessions),
-		serverURL:   serverURL,
-		repo:        repo.New(db),
-		billingRepo: billingRepo,
-		orgRepo:     orgRepo.New(db),
+		tracer:        otel.Tracer("github.com/speakeasy-api/gram/server/internal/usage"),
+		logger:        logger,
+		auth:          auth.New(logger, db, sessions),
+		serverURL:     serverURL,
+		repo:          repo.New(db),
+		billingRepo:   billingRepo,
+		orgRepo:       orgRepo.New(db),
+		posthogClient: posthogClient,
 	}
 }
 
@@ -118,6 +121,19 @@ func (s *Service) HandlePolarWebhook(w http.ResponseWriter, r *http.Request) err
 	// we force a refresh of the period usage since usage may have changed with a subscription update
 	if _, err = s.billingRepo.GetPeriodUsage(ctx, webhookPayload.Data.Customer.ExternalID); err != nil {
 		return oops.E(oops.CodeUnexpected, err, "failed to get period usage").Log(ctx, s.logger)
+	}
+
+	if err = s.posthogClient.CaptureEvent(ctx, "gram_subscription_changed", webhookPayload.Data.Customer.ExternalID, map[string]any{
+		"org_id":       webhookPayload.Data.Customer.ExternalID,
+		"org_name":     webhookPayload.Data.Customer.Name,
+		"org_slug":     webhookPayload.Data.Customer.ExternalID,
+		"is_gram":      true,
+		"product":      webhookPayload.Data.Product.Name,
+		"product_type": webhookPayload.Data.Product.Type,
+		"event":        webhookPayload.Type,
+		"email":        webhookPayload.Data.Customer.Email,
+	}); err != nil {
+		s.logger.ErrorContext(ctx, "failed to capture posthog event", attr.SlogError(err))
 	}
 
 	return nil
