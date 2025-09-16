7a986185eaf28ca11ee090cd1c3caf0979081cd6
Georges Haidar <ghaidar0@gmail.com>
2025-09-16 23:39:15 +0100

    chore: move slug pattern to constants package (#277)
    This change moves the slug pattern and regular expression to the
    `constants` package because it will be reused in application logic for
    validating Gram Functions tool names.

--- server/internal/constants/tools.go
+++ server/internal/constants/tools.go
@@ -1,5 +1,11 @@
 package constants
 
+import "regexp"
+
 const (
 	ToolTypeHTTP = "http"
 )
+
+const SlugPattern = `^[a-z0-9]+(?:[a-z0-9_-]*[a-z0-9])?$`
+
+var SlugPatternRE = regexp.MustCompile(SlugPattern)
