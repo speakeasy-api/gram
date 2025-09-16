c7c62d97d12c8032b03f4ca08e3618346d85e5ee
Georges Haidar <ghaidar0@gmail.com>
2025-09-16 22:33:37 +0000

    chore: move slug pattern to constants package
    This change moves the slug pattern and regular expression to the
    `constants` package because it will be reused in application logic for
    validating Gram Functions tool names.

--- server/design/shared/datatypes.go
+++ server/design/shared/datatypes.go
@@ -1,13 +1,12 @@
 package shared
 
 import (
+	"github.com/speakeasy-api/gram/server/internal/constants"
 	. "goa.design/goa/v3/dsl"
 )
 
-const SlugPattern = `^[a-z0-9]+(?:[a-z0-9_-]*[a-z0-9])?$`
-
 var Slug = Type("Slug", String, func() {
 	Description("A short url-friendly label that uniquely identifies a resource.")
-	Pattern(SlugPattern)
+	Pattern(constants.SlugPattern)
 	MaxLength(40)
 })
