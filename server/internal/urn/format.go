7a238ca670ee273f467533d43662f49327c47ba3
Georges Haidar <ghaidar0@gmail.com>
2025-09-17 14:09:39 +0000

    chore: add package `urn` for addressing tools
    Package `urn` introduces a naming convention for uniquely identifying
    tools across Gram clients and servers that also works well for URL
    embedding and database lookups.
    
    The format is `<kind>:<source>:<name>`, where:
    
    - **kind** the type of the tool such as `http` and `function`
    - **source**: the deployment artifact from which the tool was
    generated.
    - **name**: the name of the tool.

---
+++ server/internal/urn/format.go
@@ -0,0 +1,21 @@
+package urn
+
+// delimiter is the character that separates the parts of a URN.
+const delimiter = ":"
+
+// maxSegmentLength is the maximum length of each segment in URNs (kind, source,
+// name). Application code can further enforce shorter lengths. The purpose of
+// this is to place an upper bound on parsing logic.
+const maxSegmentLength = 128
+
+type ToolKind string
+
+const (
+	ToolKindFunction ToolKind = "function"
+	ToolKindHTTP     ToolKind = "http"
+)
+
+var toolKinds = map[ToolKind]struct{}{
+	ToolKindFunction: {},
+	ToolKindHTTP:     {},
+}
