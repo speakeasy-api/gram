686cb6b161fd7e1650d4a780b3b2ce7e194cfb15
Raymond Tukpe <rtukpe@gmail.com>
2025-09-29 15:04:55 +0200

    Update `http_requests_raw` table schema: add new fields, adjust data types, and optimize schema definitions

--- server/clickhouse/schema.sql
+++ server/clickhouse/schema.sql
@@ -1,22 +1,31 @@
-create table http_requests_raw
+create table if not exists http_requests_raw
 (
     ts                  DateTime64(3, 'UTC'),
-    organization_id     String,
-    project_id          String,
-    deployment_id       String,
-    tool_id             String,
+    organization_id     UUID,
+    project_id          UUID,
+    deployment_id       UUID,
+    tool_id             UUID,
+    tool_urn            String,
     tool_type           LowCardinality(String),
+
     trace_id            FixedString(32),
     span_id             FixedString(16),
+
     http_method         LowCardinality(String),
     http_route          String,
     status_code         UInt16,
     duration_ms         Float64,
-    request_body        Array(UInt8),
-    response_body       Array(UInt8),
-    request_headers     Array(UInt8),
-    response_headers    Array(UInt8),
+    user_agent          LowCardinality(String),
+    client_ipv4         IPv4,
+
+    request_headers     Map(String, String) CODEC (ZSTD),
+    request_body        Nullable(String) CODEC (ZSTD),
+    request_body_skip   Nullable(String),
     request_body_bytes  UInt64,
+
+    response_headers    Map(String, String) CODEC (ZSTD),
+    response_body       Nullable(String) CODEC (ZSTD),
+    response_body_skip  Nullable(String),
     response_body_bytes UInt64
 ) engine = MergeTree
       PARTITION BY toDate(ts)
