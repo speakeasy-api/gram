dcad5a00bdab6a0426c2d2bd92174ec70e219a65
Georges Haidar <ghaidar0@gmail.com>
2025-10-02 19:48:53 +0000

    f

--- functions/internal/javascript/js.go
+++ functions/internal/javascript/js.go
@@ -7,6 +7,10 @@ import (
 //go:embed gram-start.mjs
 var Entrypoint []byte
 
+// DefaultFunctions contains a default implementation of the functions.js file.
+// It is used when custom code is not found on the runner machine. The benefit
+// of having this default implementation is that it allows Gram to always get
+// a useful response even in failure scenarios.
 var DefaultFunctions = []byte(`
 export async function handleToolCall(input, context) {
   return new Response(JSON.stringify({ error: "Tool calling is not implemented" }), {
