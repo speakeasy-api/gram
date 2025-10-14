00ff5f91d891afa49db5b82931bce0f7b1c36c93
Raymond Tukpe <rtukpe@gmail.com>
2025-10-14 15:56:51 +0200

    remove redundant `Usage` fields

--- server/cmd/gram/flags_clickhouse.go
+++ server/cmd/gram/flags_clickhouse.go
@@ -5,42 +5,36 @@ import "github.com/urfave/cli/v2"
 var clickHouseFlags = []cli.Flag{
 	&cli.StringFlag{
 		Name:     "clickhouse-host",
-		Usage:    "Clickhouse Host",
 		Required: false,
 		EnvVars:  []string{"CLICKHOUSE_HOST"},
 		Value:    "localhost",
 	},
 	&cli.StringFlag{
 		Name:     "clickhouse-database",
-		Usage:    "Clickhouse Database",
 		Required: false,
 		EnvVars:  []string{"CLICKHOUSE_DATABASE"},
 		Value:    "gram",
 	},
 	&cli.StringFlag{
 		Name:     "clickhouse-username",
-		Usage:    "Clickhouse Username",
 		Required: false,
 		EnvVars:  []string{"CLICKHOUSE_USERNAME"},
 		Value:    "gram",
 	},
 	&cli.StringFlag{
 		Name:     "clickhouse-password",
-		Usage:    "Clickhouse Password",
 		Required: false,
 		EnvVars:  []string{"CLICKHOUSE_PASSWORD"},
 		Value:    "gram",
 	},
 	&cli.StringFlag{
 		Name:     "clickhouse-native-port",
-		Usage:    "Clickhouse Native Port",
 		Required: false,
 		EnvVars:  []string{"CLICKHOUSE_NATIVE_PORT"},
 		Value:    "9440",
 	},
 	&cli.BoolFlag{
 		Name:     "clickhouse-insecure",
-		Usage:    "Clickhouse Insecure",
 		Required: false,
 		EnvVars:  []string{"CLICKHOUSE_INSECURE"},
 		Value:    false,
