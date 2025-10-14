e56373e3bb68a30b6d3ba63fc8a3148b9b44ace4
Walker Lockard <walker.b.lockard@gmail.com>
2025-10-14 09:21:53 -0500

    add --json flag to whoami

--- cli/internal/app/whoami.go
+++ cli/internal/app/whoami.go
@@ -1,9 +1,11 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
+	"slices"
 	"strings"
 	"syscall"
 
@@ -13,6 +15,7 @@ import (
 	"github.com/speakeasy-api/gram/cli/internal/flags"
 	"github.com/speakeasy-api/gram/cli/internal/profile"
 	"github.com/speakeasy-api/gram/cli/internal/workflow"
+	"github.com/speakeasy-api/gram/server/gen/keys"
 	"github.com/urfave/cli/v2"
 )
 
@@ -27,6 +30,7 @@ If no profile is configured, the command will indicate that no profile is set up
 		Flags: []cli.Flag{
 			flags.APIEndpoint(),
 			flags.APIKey(),
+			flags.JSON(),
 		},
 		Action: func(c *cli.Context) error {
 			ctx, cancel := signal.NotifyContext(
@@ -55,58 +59,115 @@ If no profile is configured, the command will indicate that no profile is set up
 				return fmt.Errorf("failed to verify API key: %w", err)
 			}
 
-			headerStyle := lipgloss.NewStyle().
-				Bold(true).
-				Foreground(lipgloss.Color("6"))
-
-			fmt.Println()
-			fmt.Println(headerStyle.Render("Profile Information"))
-
-			orgTable := table.New().
-				Border(lipgloss.HiddenBorder()).
-				StyleFunc(func(row, col int) lipgloss.Style {
-					if col == 0 {
-						return headerStyle
-					}
-					return lipgloss.NewStyle()
-				}).
-				Headers("ORGANIZATION DETAILS").
-				Rows(
-					[]string{"Name:", result.Organization.Name},
-					[]string{"ID:", result.Organization.ID},
-					[]string{"Slug:", result.Organization.Slug},
-				)
-
-			fmt.Println(orgTable.Render())
-
-			fmt.Println(headerStyle.MarginLeft(1).MarginBottom(1).Render("API KEY SCOPES"))
-
-			fmt.Println(lipgloss.NewStyle().MarginLeft(1).Bold(true).Render(strings.Join(result.Scopes, ", ")))
-
-			projectTable := table.New().
-				Border(lipgloss.HiddenBorder()).
-				StyleFunc(func(row, col int) lipgloss.Style {
-					if row <= 0 {
-						return headerStyle
-					}
-
-					if col == 0 {
-						return lipgloss.NewStyle().Bold(true)
-					}
-
-					return lipgloss.NewStyle()
-				}).
-				Headers("PROJECTS").
-				Row("Name", "ID", "Slug")
-
-			for _, proj := range result.Projects {
-				projectTable.Row(proj.Name, proj.ID, proj.Slug)
+			if c.Bool("json") {
+				out, err := json.MarshalIndent(ProfileInfo{
+					Organization:       *result.Organization,
+					Scopes:             result.Scopes,
+					CurrentProjectSlug: workflowParams.ProjectSlug,
+					Projects:           result.Projects,
+				}, "", "  ")
+				if err != nil {
+					return fmt.Errorf("failed to encode profile info: %w", err)
+				}
+				fmt.Println(string(out))
+				return nil
 			}
 
-			fmt.Println(projectTable.Render())
+			printProfile(ProfileInfo{
+				Organization:       *result.Organization,
+				Scopes:             result.Scopes,
+				CurrentProjectSlug: workflowParams.ProjectSlug,
+				Projects:           result.Projects,
+			})
 
 			return nil
 		},
 	}
+}
 
+type ProfileInfo struct {
+	Organization       keys.ValidateKeyOrganization
+	Scopes             []string
+	CurrentProjectSlug string
+	Projects           []*keys.ValidateKeyProject
+}
+
+func printProfile(profile ProfileInfo) {
+	headerStyle := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("6"))
+
+	fmt.Println()
+	fmt.Println(headerStyle.Render("Profile Information"))
+
+	orgTable := table.New().
+		Border(lipgloss.HiddenBorder()).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			if col == 0 {
+				return headerStyle
+			}
+			return lipgloss.NewStyle()
+		}).
+		Headers("ORGANIZATION DETAILS").
+		Rows(
+			[]string{"Name:", profile.Organization.Name},
+			[]string{"Slug:", profile.Organization.Slug},
+		)
+
+	fmt.Println(orgTable.Render())
+
+	currentProjectIdx := slices.IndexFunc(profile.Projects, func(p *keys.ValidateKeyProject) bool {
+		return p.Slug == profile.CurrentProjectSlug
+	})
+
+	currentProjectTable := table.New().
+		Border(lipgloss.HiddenBorder()).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			if col == 0 {
+				return headerStyle
+			}
+			return lipgloss.NewStyle()
+		}).
+		Headers("TARGET PROJECT DETAILS").
+		Rows(
+			[]string{"Name:", profile.Projects[currentProjectIdx].Name},
+			[]string{"Slug:", profile.Projects[currentProjectIdx].Slug},
+		)
+
+	fmt.Println(currentProjectTable.Render())
+
+	fmt.Println(headerStyle.Margin(1).Render("API KEY SCOPES"))
+
+	fmt.Println(lipgloss.NewStyle().MarginLeft(1).MarginBottom(1).Bold(true).Render(strings.Join(profile.Scopes, ", ")))
+
+	projectTable := table.New().
+		Border(lipgloss.HiddenBorder()).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			if row <= 0 {
+				return headerStyle
+			}
+
+			if col == 0 {
+				return lipgloss.NewStyle().Bold(true)
+			}
+
+			return lipgloss.NewStyle()
+		}).
+		Headers("ALL PROJECTS").
+		Row("Name", "Slug")
+
+	for _, proj := range profile.Projects {
+		projectTable.Row(proj.Name, proj.Slug)
+	}
+
+	fmt.Println(projectTable.Render())
+}
+
+func printProfileJSON(profile *ProfileInfo) error {
+	out, err := json.MarshalIndent(profile, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to encode profile info: %w", err)
+	}
+	fmt.Println(string(out))
+	return nil
 }
