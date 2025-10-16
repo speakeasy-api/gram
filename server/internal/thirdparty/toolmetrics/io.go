5426089c36c70b398a6d24cfd63062cdf3c933a3
Raymond Tukpe <rtukpe@gmail.com>
2025-10-16 11:14:51 +0200

    avoid wrapping io.EOF in countingReadCloser Read method

--- server/internal/thirdparty/toolmetrics/io.go
+++ server/internal/thirdparty/toolmetrics/io.go
@@ -17,16 +17,17 @@ func NewCountingReadCloser(rc io.ReadCloser, onClose func(int)) io.ReadCloser {
 	return &countingReadCloser{
 		ReadCloser: rc,
 		onClose:    onClose,
+		count:      0,
 	}
 }
 
+// Read implements io.ReadCloser. The error is not wrapped
+// wrapcheck also doesn't support wrapping io.EOF: https://github.com/tomarrell/wrapcheck/issues/39
+// io.EOF is treated as a sentinel value and must not be wrapped: https://github.com/golang/go/issues/39155
 func (crc *countingReadCloser) Read(p []byte) (n int, err error) {
 	n, err = crc.ReadCloser.Read(p)
 	crc.count += n
-	if err != nil && err != io.EOF {
-		return n, fmt.Errorf("counting read: %w", err) // for other errors aside from EOF, return the error
-	}
-	return n, err // preserves io.EOF
+	return n, err //nolint:wrapcheck // a lot of Go code in the wild still relies on `err == io.EOF` instead of `errors.Is(err, io.EOF)` so we should avoid wrapping.
 }
 
 func (crc *countingReadCloser) Close() error {
