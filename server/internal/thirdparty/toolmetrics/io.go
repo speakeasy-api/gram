d599f0b0c088849b9c0961f92cd1745cc380b31f
Raymond Tukpe <rtukpe@gmail.com>
2025-10-16 10:35:23 +0200

    improve error handling in countingReadCloser

--- server/internal/thirdparty/toolmetrics/io.go
+++ server/internal/thirdparty/toolmetrics/io.go
@@ -5,18 +5,17 @@ import (
 	"io"
 )
 
-// countingReadCloser wraps a ReadCloser and counts bytes as they're read
+// countingReadCloser wraps an io.ReadCloser and counts the total bytes read.
 type countingReadCloser struct {
 	io.ReadCloser
 	count   int
 	onClose func(int)
 }
 
-// NewCountingReadCloser creates a new countingReadCloser that counts bytes as they're read
+// NewCountingReadCloser creates a new countingReadCloser that counts bytes as they're read.
 func NewCountingReadCloser(rc io.ReadCloser, onClose func(int)) io.ReadCloser {
 	return &countingReadCloser{
 		ReadCloser: rc,
-		count:      0,
 		onClose:    onClose,
 	}
 }
@@ -24,22 +23,19 @@ func NewCountingReadCloser(rc io.ReadCloser, onClose func(int)) io.ReadCloser {
 func (crc *countingReadCloser) Read(p []byte) (n int, err error) {
 	n, err = crc.ReadCloser.Read(p)
 	crc.count += n
-	if err != nil {
-		return n, fmt.Errorf("counting read: %w", err)
+	if err != nil && err != io.EOF {
+		return n, fmt.Errorf("counting read: %w", err) // for other errors aside from EOF, return the error
 	}
-
-	return n, nil
+	return n, err // preserves io.EOF
 }
 
 func (crc *countingReadCloser) Close() error {
+	err := crc.ReadCloser.Close()
 	if crc.onClose != nil {
 		crc.onClose(crc.count)
 	}
-
-	err := crc.ReadCloser.Close()
 	if err != nil {
 		return fmt.Errorf("close counting reader: %w", err)
 	}
-
 	return nil
 }
