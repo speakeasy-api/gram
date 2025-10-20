d15ae3ba2ce5d4aa3a4fb201bf5247fab347b194
Georges Haidar <ghaidar0@gmail.com>
2025-10-20 17:52:05 +0000

    feat: integrate build with Gram CLI to create deployments
    This change updates the templated build script in the mini-framework
    template to detect and invoke the Gram CLI and create deployments. Long
    term, this logic will be moved to `@gram-ai/functions` package instead
    of live in user projects.

--- ts-framework/create-function/gram-template-gram/src/build.ts
+++ ts-framework/create-function/gram-template-gram/src/build.ts
@@ -1,5 +1,7 @@
+import { join } from "node:path";
 import { buildFunctions } from "@gram-ai/functions/build";
-import { join } from "path";
+import { $, chalk } from "zx";
+import pkg from "../package.json" with { type: "json" };
 
 async function build() {
   await buildFunctions({
@@ -7,8 +9,36 @@ async function build() {
     entrypoint: join(import.meta.dirname, "functions.ts"),
     export: "default",
   });
+
+  const cmd = process.platform === "win32" ? ["where"] : ["command", "-v"];
+  const program = "gram";
+  const gramPath = await $`${cmd} ${program}`.nothrow();
+
+  if (gramPath.exitCode !== 0) {
+    throw new Error(
+      `Gram CLI not found. Please install it from https://www.speakeasy.com/docs/gram/command-line/installation.`,
+    );
+  }
+
+  const slug = pkg.name.split("/").pop();
+  if (!slug) {
+    throw new Error(
+      `Could not determine function slug from package.json#name.`,
+    );
+  }
+
+  console.log(chalk.greenBright(`Staging dist/gram.zip with slug: ${slug}`));
+  await $`gram stage function --slug ${slug} --location dist/gram.zip`;
+
+  console.log(chalk.greenBright(`Deploying with Gram CLI...`));
+  await $({
+    stdio: ["inherit", "inherit", "inherit"],
+  })`gram push --config gram.json`;
 }
 
 if (import.meta.main) {
-  await build();
+  await build().catch((err) => {
+    console.error(err instanceof Error ? err.message : String(err));
+    process.exit(1);
+  });
 }
