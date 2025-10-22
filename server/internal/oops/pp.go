0c41512246c365be72666d56f60bf53b6a0c7a09
Georges Haidar <ghaidar0@gmail.com>
2025-10-22 15:24:12 +0000

    fix: correctly wrap slog.Logger while capturing source
    This change updates oops.ShareableError so that the source location is
    correctly captured when wrapping the logger in the
    `oops.ShareableError.Log()` method.
    
    ```diff lang=json
    {
      "time": "2025-10-22T15:21:00.738859927Z",
      "level": "ERROR",
    - "source": {
    -   "function": "github.com/speakeasy-api/gram/server/internal/oops.(*ShareableError).Log",
    -   "file": "/home/disintegrator/github.com/speakeasy-api/gram/server/internal/oops/pp.go",
    -   "line": 132
    - },
    + "source": {
    +   "function": "github.com/speakeasy-api/gram/server/internal/deployments.amendDeployment",
    +   "file": "/home/disintegrator/github.com/speakeasy-api/gram/server/internal/deployments/crud.go",
    +   "line": 242
    + },
      "msg": "error adding deployment functions asset",
      "git.commit.sha": "31f4cd016ec8adf0d2778545ac2300425fb67d73",
      "git.repository_url": "github.com/speakeasy-api/gram",
      "service.name": "gram-server",
      "service.version": "31f4cd01",
      "deployment.environment.name": "local",
      "gram.component": "deployments",
      "gram.project.id": "0199ed2e-7215-7bbb-a216-ff5e3d5b8ff5",
      "gram.deployment.id": "019a0c82-5a32-73a2-9f99-b35f040fa32b",
      "gram.error.id": "ftyz831Z",
      "error.message": "...",
      "trace.id": "d2b46d7c75b8ed7151ee83846cb6363e",
      "span.id": "97b1a1c503890261",
      "goa.service": "deployments",
      "goa.method": "evolve"
    }
    ```

--- server/internal/oops/pp.go
+++ server/internal/oops/pp.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"runtime"
 	"sync"
+	"time"
 
 	"github.com/speakeasy-api/gram/server/internal/attr"
 	"github.com/speakeasy-api/gram/server/internal/conv"
@@ -25,9 +26,6 @@ var funcMemo sync.Map
 //
 //go:noinline
 func E(code Code, cause error, public string, args ...any) *ShareableError {
-	var pc [1]uintptr
-	runtime.Callers(2, pc[:])
-
 	msg := public
 	if len(args) > 0 {
 		msg = fmt.Sprintf(public, args...)
@@ -39,7 +37,6 @@ func E(code Code, cause error, public string, args ...any) *ShareableError {
 		cause:   cause,
 		private: "",
 		public:  msg,
-		pc:      pc[0],
 	}
 }
 
@@ -49,16 +46,12 @@ func E(code Code, cause error, public string, args ...any) *ShareableError {
 //
 //go:noinline
 func C(code Code) *ShareableError {
-	var pc [1]uintptr
-	runtime.Callers(2, pc[:])
-
 	return &ShareableError{
 		Code:    code,
 		id:      goa.NewErrorID(),
 		cause:   nil,
 		private: "",
 		public:  code.UserMessage(),
-		pc:      pc[0],
 	}
 }
 
@@ -72,7 +65,6 @@ type ShareableError struct {
 	cause   error
 	public  string
 	private string
-	pc      uintptr
 }
 
 // Error implements the error interface.
@@ -89,10 +81,10 @@ func (e *ShareableError) String() string {
 	}
 
 	if e.cause == nil {
-		return fmt.Sprintf("%s [%s]", msg, funcForPC(e.pc))
+		return msg
 	}
 
-	return fmt.Sprintf("%s: %s [%s]", msg, e.cause.Error(), funcForPC(e.pc))
+	return fmt.Sprintf("%s: %s", msg, e.cause.Error())
 }
 
 // Unwrap returns the underlying cause of the error, if any.
@@ -123,14 +115,20 @@ func (e *ShareableError) LogValue() slog.Value {
 // Log logs the error using the provided logger and context. It also sets the
 // OpenTelemetry span status to error. Additional arguments can be provided to
 // include more context in the log entry.
-func (e *ShareableError) Log(ctx context.Context, logger *slog.Logger, args ...any) *ShareableError {
+func (e *ShareableError) Log(ctx context.Context, logger *slog.Logger, args ...slog.Attr) *ShareableError {
 	trace.SpanFromContext(ctx).SetStatus(codes.Error, e.String())
 
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Log]
+	r := slog.NewRecord(time.Now(), slog.LevelError, e.public, pcs[0])
+
 	if len(args) > 0 {
-		logger.ErrorContext(ctx, e.public, append(args, attr.SlogErrorID(e.id), attr.SlogErrorMessage(e.String()))...)
+		r.AddAttrs(append(args, attr.SlogErrorID(e.id), attr.SlogErrorMessage(e.String()))...)
 	} else {
-		logger.ErrorContext(ctx, e.public, attr.SlogErrorID(e.id), attr.SlogErrorMessage(e.String()))
+		r.AddAttrs(attr.SlogErrorID(e.id), attr.SlogErrorMessage(e.String()))
 	}
+
+	logger.Handler().Handle(ctx, r)
 	return e
 }
 
@@ -163,25 +161,4 @@ func (e *ShareableError) AsGoa() *goa.ServiceError {
 // Error.
 func (e *ShareableError) HTTPStatus() int {
 	return conv.Default(StatusCodes[e.Code], http.StatusInternalServerError)
-}
-
-func funcForPC(pc uintptr) string {
-	if f, ok := funcMemo.Load(pc); ok {
-		val, ok := f.(string)
-		if !ok {
-			panic(fmt.Sprintf("funcForPC: expected string, got %T", f))
-		}
-		return val
-	}
-
-	fnc := runtime.FuncForPC(pc)
-	if fnc == nil {
-		funcMemo.Store(pc, "")
-		return ""
-	}
-
-	file, line := fnc.FileLine(pc)
-	loc := fmt.Sprintf("%s:%d", file, line)
-	funcMemo.Store(pc, loc)
-	return loc
 }
