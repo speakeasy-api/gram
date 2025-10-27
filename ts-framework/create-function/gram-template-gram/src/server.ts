9fd9e4ee78d93a837b780e868ccdb21103cc0907
Georges Haidar <ghaidar0@gmail.com>
2025-10-27 17:32:05 +0000

    feat: provide an MCP server for local development
    This change updates the Gram Functions framework template to include an
    MCP server for local development that wraps a `Gram` instance. This
    will provide developers a better feedback loop when developing tools
    before deploying them to Gram.

--- ts-framework/create-function/gram-template-gram/src/server.ts
+++ ts-framework/create-function/gram-template-gram/src/server.ts
@@ -1,44 +1,125 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import {
+  CallToolRequestSchema,
+  ListToolsRequestSchema,
+  type CallToolResult,
+  type ListToolsResult,
+} from "@modelcontextprotocol/sdk/types.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import pkg from "../package.json" with { type: "json" };
 import { gram } from "./gram.ts";
 
-const app = new Hono();
+const structuredLike = /\b(yaml|yml|json|toml|xml|xhtml)\b/i;
+const textLike = /^text\//i;
+const imageLike = /^image\//i;
+const audioLike = /^audio\//i;
 
-app.post("/tool-call", async (c) => {
-  const req = await c.req.json();
-  return await gram.handleToolCall(req, {
-    signal: AbortSignal.timeout(1 * 60 * 1000),
-  });
-});
+export const server = new Server(
+  {
+    name: pkg.name,
+    version: pkg.version,
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  },
+);
 
-app.get("/manifest", async () => {
-  const manifest = gram.manifest();
-  return new Response(JSON.stringify(manifest, null, 2), {
-    headers: { "Content-Type": "application/json" },
-  });
-});
+server.setRequestHandler(
+  ListToolsRequestSchema,
+  async (): Promise<ListToolsResult> => {
+    const tools = gram.manifest().tools.map((t) => {
+      return {
+        name: t.name,
+        description: t.description,
+        inputSchema: t.inputSchema,
+      };
+    }) as ListToolsResult["tools"];
 
-export function startServer(port: number = 3000) {
-  const server = serve({ fetch: app.fetch, port }, (info) => {
-    console.log(`Listening on http://localhost:${info.port}`);
-  });
+    return {
+      tools,
+    };
+  },
+);
 
-  const onClose = () => {
-    console.log("Shutting down server...");
-    server.close((err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      process.exit(0);
+server.setRequestHandler(
+  CallToolRequestSchema,
+  async (req, extra): Promise<CallToolResult> => {
+    const { name, arguments: args } = req.params;
+
+    const resp = await gram.handleToolCall({ name, input: args } as any, {
+      signal: extra.signal,
     });
+
+    let ctype = resp.headers.get("Content-Type") || "";
+    ctype = ctype.split(";")[0]?.trim() || "";
+
+    switch (true) {
+      case textLike.test(ctype) || structuredLike.test(ctype): {
+        const text = await resp.text();
+        return {
+          content: [{ type: "text", text }],
+        };
+      }
+      case imageLike.test(ctype): {
+        return {
+          content: [
+            {
+              type: "image",
+              mimeType: ctype,
+              data: await responseToBase64(resp),
+            },
+          ],
+        };
+      }
+      case audioLike.test(ctype): {
+        return {
+          content: [
+            {
+              type: "audio",
+              mimeType: ctype,
+              data: await responseToBase64(resp),
+            },
+          ],
+        };
+      }
+      default: {
+        return {
+          isError: true,
+          content: [
+            {
+              type: "text",
+              text: `Unhandled content type: ${ctype}. Create a handler for this type in the MCP server.`,
+            },
+          ],
+        };
+      }
+    }
+  },
+);
+
+async function responseToBase64(resp: Response): Promise<string> {
+  const blob = await resp.arrayBuffer();
+  const buffer = Buffer.from(blob);
+  return buffer.toString("base64");
+}
+
+async function run() {
+  console.error("Starting MCP server with stdio...");
+  const stdio = new StdioServerTransport();
+  await server.connect(stdio);
+
+  const quit = async () => {
+    console.error("\nShutting down MCP server...");
+    await server.close();
+    process.exit(0);
   };
-  process.on("SIGINT", onClose);
-  process.on("SIGTERM", onClose);
+  process.once("SIGINT", quit);
+  process.once("SIGTERM", quit);
 }
 
 if (import.meta.main) {
-  startServer();
+  run();
 }
-
-export default app;
