c296cd50e2412a2c4b66fea4ac8201892e1b053c
farazcsk <farazcsk@gmail.com>
2025-10-29 16:17:20 +0000

    fix: remove accidentally commited docs files

--- docs/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_pNhFH from "../../assets/shader-book-light.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/shader-book-light.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_pNhFH]]);
-		
