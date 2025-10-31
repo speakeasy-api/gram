bdc36254d49fa4ac11de8e64f3700c7b38dec7ac
Adam Bull <adam@speakeasy.com>
2025-10-31 12:23:20 +0000

    fix: use a single react version in the dashboard

--- client/dashboard/vite.config.ts
+++ client/dashboard/vite.config.ts
@@ -60,6 +60,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      react: path.resolve(__dirname, "node_modules/react"),
     },
   },
 });
