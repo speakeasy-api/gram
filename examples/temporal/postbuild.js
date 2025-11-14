db1c4a99467a886d3bff4ab44b85c4098f1ca44c
Ryan Albert <ryantimalbert@gmail.com>
2025-11-14 15:42:28 -0800

    feat: comment

--- examples/temporal/postbuild.js
+++ examples/temporal/postbuild.js
@@ -4,6 +4,7 @@ import { execSync } from 'child_process';
 // Modify functions.js
 const file = 'dist/functions.js';
 const content = readFileSync(file, 'utf8');
+// we add this to be able to handle some non ESM compatible imports with temporal's
 const prepend = "import { createRequire } from 'module'; const require = createRequire(import.meta.url);\n";
 writeFileSync(file, prepend + content);
 
