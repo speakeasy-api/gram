a6c6dd5722769f51c95d2bafbcb48db438163137
Nolan Di Mare Sullivan <nolan.dm.sullivan@gmail.com>
2025-11-17 14:08:54 +0000

    Run prettier

--- client/dashboard/src/pages/login/components/login-section.tsx
+++ client/dashboard/src/pages/login/components/login-section.tsx
@@ -101,7 +101,8 @@ function AuthLayout({ children }: { children: React.ReactNode }) {
         <div className="flex flex-col items-center gap-4">
           <GramLogo className="w-25 mb-2 dark:!invert-0" variant="vertical" />
           <p className="text-body-lg text-center dark:text-black">
-            The fastest path to production MCP. Create, curate and host high quality agent tools as MCP servers.
+            The fastest path to production MCP. Create, curate and host high
+            quality agent tools as MCP servers.
           </p>
         </div>
 
