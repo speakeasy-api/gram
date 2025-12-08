8e942445e605f9b51a55b6ec98837922470db126
Georges Haidar <ghaidar0@gmail.com>
2025-12-08 14:37:16 +0000

    chore: implement S3 asset store
    This change implements the asset store interface for S3-compatible
    storage backends. This covers support for Tigris which will eventually
    be used to power large Gram Functions.

--- server/internal/assets/impl.go
+++ server/internal/assets/impl.go
@@ -538,7 +538,7 @@ type uploadAssetParams struct {
 
 func (s *Service) uploadAsset(ctx context.Context, params *uploadAssetParams) (*url.URL, error) {
 	projectID := params.projectID
-	dst, uri, err := s.storage.Write(ctx, path.Join(projectID.String(), params.filename), params.file, params.contentType)
+	dst, uri, err := s.storage.Write(ctx, path.Join(projectID.String(), params.filename), params.contentType, params.contentLength)
 	if err != nil {
 		return nil, oops.E(oops.CodeUnexpected, fmt.Errorf("write to blob storage: %w", err), "error writing document")
 	}
