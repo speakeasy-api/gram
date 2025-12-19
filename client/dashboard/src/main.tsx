679a2ac0217ddd62896748dd16cc4fe5ac110b11
Walker Lockard <walker.b.lockard@gmail.com>
2025-12-19 08:42:06 -0600

    remove FE arbitrary comment

--- client/dashboard/src/main.tsx
+++ client/dashboard/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "@/components/ui/sonner";
 import App from "./App.tsx";
 
-// boop
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <style>
