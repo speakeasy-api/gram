b9b206101eb5a44abcf967b64859a123d97bfea4
Georges Haidar <ghaidar0@gmail.com>
2026-01-07 14:43:27 +0000

    chore: allow elements storybook to run with SSL
    This change updates the storybook dev server for Elements to:
    
    - Accept SSL key and certificate file paths via existing environment variables and enable HTTPS for the dev server.
    - Allow hosts other than localhost including 'devbox' and 127.0.0.1 for better compatibility in various dev environments.

--- elements/.storybook/main.ts
+++ elements/.storybook/main.ts
@@ -3,6 +3,14 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs'],
+  viteFinal: (config) => {
+    config.server = {
+      ...config.server,
+      allowedHosts: ['localhost', '127.0.0.1', 'devbox'],
+    }
+
+    return config
+  },
   framework: {
     name: '@storybook/react-vite',
     options: {
