a5dddfa55be669b3c640fec4e0eac93a6d2cd0b5
tgmendes <tiago@speakeasy.com>
2026-01-29 16:04:19 -0800

    fix: add lint ignore rules to all drop statements

--- server/clickhouse/migrations/20260129234507_drop-telemetry-http-cols.sql
+++ server/clickhouse/migrations/20260129234507_drop-telemetry-http-cols.sql
@@ -1,5 +1,8 @@
 -- atlas:nolint destructive
 ALTER TABLE `telemetry_logs` DROP COLUMN `http_request_method`;
+-- atlas:nolint destructive
 ALTER TABLE `telemetry_logs` DROP COLUMN `http_response_status_code`;
+-- atlas:nolint destructive
 ALTER TABLE `telemetry_logs` DROP COLUMN `http_route`;
+-- atlas:nolint destructive
 ALTER TABLE `telemetry_logs` DROP COLUMN `http_server_url`;
