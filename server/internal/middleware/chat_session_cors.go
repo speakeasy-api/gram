6254a75b1b3c3562ef72892d0216d46140f1bd2d
tgmendes <tiago@speakeasy.com>
2026-02-04 14:11:33 +0000

    chore: ensure only chat. is included in cors

--- server/internal/middleware/chat_session_cors.go
+++ server/internal/middleware/chat_session_cors.go
@@ -13,7 +13,7 @@ import (
 var chatSessionsAllowedRoutes = []string{
 	"/chat/completions",
 	"/mcp",
-	"/rpc/chat",
+	"/rpc/chat.",
 }
 
 // This isn't practical to do as a proper middleware because it needs to interoperate with the CORSMiddleware which does things like returning early for OPTIONS requests.
