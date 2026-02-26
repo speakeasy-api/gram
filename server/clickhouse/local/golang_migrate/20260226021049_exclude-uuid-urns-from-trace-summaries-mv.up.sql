15b5d4e8b367d1b8cb23ed5e33be3b35e3fbc80b
Quinn Stearns <quinn@speakeasy.com>
2026-02-25 18:14:42 -0800

    chore: reorder clickhouse migrations to be linear

---
+++ server/clickhouse/local/golang_migrate/20260226021049_exclude-uuid-urns-from-trace-summaries-mv.up.sql
@@ -0,0 +1,4 @@
+-- drop "trace_summaries_mv" view
+DROP VIEW `trace_summaries_mv`;
+-- create "trace_summaries_mv" view
+CREATE MATERIALIZED VIEW `trace_summaries_mv` TO `trace_summaries` AS SELECT trace_id, gram_project_id, any(gram_deployment_id) AS gram_deployment_id, any(gram_function_id) AS gram_function_id, any(gram_urn) AS gram_urn, min(time_unix_nano) AS start_time_unix_nano, toUInt64(count(*)) AS log_count, anyIfState(toInt32OrNull(toString(attributes.http.response.status_code)), toString(attributes.http.response.status_code) != '') AS http_status_code FROM telemetry_logs WHERE (trace_id IS NOT NULL) AND (trace_id != '') AND (NOT startsWith(telemetry_logs.gram_urn, 'urn:uuid:')) GROUP BY trace_id, gram_project_id;
