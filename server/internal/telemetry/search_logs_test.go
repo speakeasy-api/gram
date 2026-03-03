7a391e952f73aed0b791e196d52bf83dbe061c77
chasecrumbaugh <chasecrumbaugh4@gmail.com>
2026-03-03 11:35:47 -0800

    change to ptr

--- server/internal/telemetry/search_logs_test.go
+++ server/internal/telemetry/search_logs_test.go
@@ -1366,6 +1366,7 @@ func insertTelemetryLogWithParams(t *testing.T, ctx context.Context, params test
 // ptr returns a pointer to the given value
 //
 //go:fix inline
+//go:fix inline
 func ptr[T any](v T) *T {
 	return new(v)
 }
