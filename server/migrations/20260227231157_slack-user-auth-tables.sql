7ded388b09f11fa24667baeb1f45899294a3b6fc
Quinn Stearns <quinn@speakeasy.com>
2026-03-05 12:21:03 -0800

    fix: better atlas annotations

--- server/migrations/20260227231157_slack-user-auth-tables.sql
+++ server/migrations/20260227231157_slack-user-auth-tables.sql
@@ -1,4 +1,3 @@
--- atlas:nolint DS102
 -- Create "slack_pending_auths" table
 CREATE TABLE "slack_pending_auths" (
   "id" uuid NOT NULL DEFAULT gen_random_uuid(),
@@ -24,5 +23,6 @@ CREATE TABLE "slack_user_mappings" (
   CONSTRAINT "slack_user_mappings_slack_app_id_slack_user_id_key" UNIQUE ("slack_app_id", "slack_user_id"),
   CONSTRAINT "slack_user_mappings_slack_app_id_fkey" FOREIGN KEY ("slack_app_id") REFERENCES "slack_apps" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
 );
+-- atlas:nolint destructive
 -- Drop "slack_app_connections" table
 DROP TABLE "slack_app_connections";
