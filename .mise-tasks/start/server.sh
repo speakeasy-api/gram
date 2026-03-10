87768f3b5e90df20e934d6fbe03309457438d24a
chasecrumbaugh <chasecrumbaugh4@gmail.com>
2026-03-10 14:53:46 -0700

    fixes

--- .mise-tasks/start/server.sh
+++ .mise-tasks/start/server.sh
@@ -10,15 +10,4 @@ if [ -f "../config.local.toml" ]; then
     CONFIG_ARGS=(--config-file ../config.local.toml)
 fi
 
-<<<<<<< HEAD
-# Check if air should be disabled
-if [ "${GRAM_DISABLE_AIR:-0}" = "1" ]; then
-    # Run directly without air
-    mise run -q build:server && ./bin/gram start "${CONFIG_ARGS[@]}" "$@"
-else
-    # Use air for hot reload - args after -- are passed to the binary
-    air -- start "${CONFIG_ARGS[@]}" "$@"
-fi
-=======
 go run -ldflags="-X github.com/speakeasy-api/gram/server/cmd/gram.GitSHA=${GIT_SHA} -X goa.design/clue/health.Version=${GIT_SHA}" main.go start "${CONFIG_ARGS[@]}" "$@"
->>>>>>> main
--- .mise-tasks/start/worker.sh
+++ .mise-tasks/start/worker.sh
@@ -2,16 +2,5 @@
 #MISE dir="{{ config_root }}/server"
 #MISE description="Start up the Temporal worker"
 
-<<<<<<< HEAD
-# Check if air should be disabled
-if [ "${GRAM_DISABLE_AIR:-0}" = "1" ]; then
-    # Run directly without air
-    mise run -q build:server && ./bin/gram worker "$@"
-else
-    # Use air for hot reload - args after -- are passed to the binary
-    air -- worker "$@"
-fi
-=======
 GIT_SHA=$(git rev-parse HEAD)
 go run -ldflags="-X github.com/speakeasy-api/gram/server/cmd/gram.GitSHA=${GIT_SHA} -X goa.design/clue/health.Version=${GIT_SHA}" main.go worker
->>>>>>> main
