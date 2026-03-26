fb655537fffb9aef6e9157bc2162d6139a365447
chasecrumbaugh <chasecrumbaugh4@gmail.com>
2026-03-26 11:54:03 -0700

    mig: new columns to trace_summaries MV

---
+++ server/clickhouse/local/golang_migrate/20260326185340_add-user-email-hook-source-to-trace-summaries.down.sql
@@ -0,0 +1,6 @@
+-- reverse: create "trace_summaries_mv" view
+DROP VIEW `trace_summaries_mv`;
+-- reverse: drop "trace_summaries_mv" view
+CREATE MATERIALIZED VIEW `trace_summaries_mv` TO `trace_summaries` AS SELECT trace_id, gram_project_id, any(gram_deployment_id) AS gram_deployment_id, any(gram_function_id) AS gram_function_id, any(gram_urn) AS gram_urn, any(tool_name) AS tool_name, any(tool_source) AS tool_source, any(event_source) AS event_source, min(time_unix_nano) AS start_time_unix_nano, toUInt64(count(*)) AS log_count, anyIfState(toInt32OrNull(toString(attributes.http.response.status_code)), toString(attributes.http.response.status_code) != '') AS http_status_code FROM telemetry_logs WHERE (trace_id IS NOT NULL) AND (trace_id != '') AND (NOT startsWith(telemetry_logs.gram_urn, 'urn:uuid:')) GROUP BY trace_id, gram_project_id;
+ALTER TABLE `trace_summaries` DROP COLUMN `hook_source`;
+ALTER TABLE `trace_summaries` DROP COLUMN `user_email`;
