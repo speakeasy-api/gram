ef1494efc13825fbb1e94b422f31585da8d50d2e
chasecrumbaugh <chasecrumbaugh4@gmail.com>
2026-03-30 14:34:26 -0700

    regen sdk

--- client/sdk/src/react-query/index.ts
+++ client/sdk/src/react-query/index.ts
@@ -95,6 +95,7 @@ export * from "./mcpMetadataSet.js";
 export * from "./mcpRegistriesClearCache.js";
 export * from "./mcpRegistriesGetServerDetails.js";
 export * from "./project.js";
+export * from "./projectsSetOrganizationWhitelist.js";
 export * from "./publishPackage.js";
 export * from "./redeployDeployment.js";
 export * from "./register.js";
