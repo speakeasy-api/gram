8b00a721594404568f33c36bc004c17dc89db915
tgmendes <tiago@speakeasy.com>
2026-04-08 14:49:14 +0100

    feat: expose current user access grants

--- client/sdk/src/lib/config.ts
+++ client/sdk/src/lib/config.ts
@@ -56,7 +56,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.0.1",
-  sdkVersion: "0.32.28",
+  sdkVersion: "0.32.29",
   genVersion: "2.844.3",
-  userAgent: "speakeasy-sdk/typescript 0.32.28 2.844.3 0.0.1 @gram/client",
+  userAgent: "speakeasy-sdk/typescript 0.32.29 2.844.3 0.0.1 @gram/client",
 } as const;
