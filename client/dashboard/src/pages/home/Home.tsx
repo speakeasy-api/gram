1dcca519fead363e0babec2635b7e8e90be6b419
Alex Martin <alex.martin@speakeasy.com>
2026-04-14 14:59:42 -0700

    remove ProjectOnboarding

--- client/dashboard/src/pages/home/Home.tsx
+++ client/dashboard/src/pages/home/Home.tsx
@@ -1,23 +1,14 @@
 import { Page } from "@/components/page-layout";
-import { useListToolsets } from "@gram/client/react-query";
 import { ProjectDashboard } from "@/components/project/ProjectDashboard";
-import { ProjectOnboarding } from "@/components/project/ProjectOnboarding";
 
 export default function Home() {
-  const { data } = useListToolsets();
-  const hasToolsets = (data?.toolsets?.length ?? 0) > 0;
-
   return (
     <Page>
       <Page.Header>
         <Page.Header.Breadcrumbs />
       </Page.Header>
       <Page.Body>
-        {hasToolsets ? (
-          <ProjectDashboard />
-        ) : (
-          <ProjectOnboarding toolsets={data?.toolsets} />
-        )}
+        <ProjectDashboard />
       </Page.Body>
     </Page>
   );
