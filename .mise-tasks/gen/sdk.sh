acfbe6a9cfa092c1f7645e243c7ae9e83e6d7898
Brian Flad <bflad417@gmail.com>
2026-04-17 14:36:47 -0400

    chore: Also use --skip-upload-spec for CI

--- .mise-tasks/gen/sdk.sh
+++ .mise-tasks/gen/sdk.sh
@@ -4,6 +4,7 @@
 
 #USAGE flag "-c --check" help="Check if the Gram-Internal OpenAPI output is up-to-date"
 #USAGE flag "-s --skip-versioning" help="Skip automatic SDK version increments"
+#USAGE flag "-u --skip-upload-spec" help="Skip uploading the spec to the Speakeasy registry"
 
 set -e
 
@@ -11,6 +12,9 @@ generate() {
   args=()
   if [[ "${usage_skip_versioning:-}" == "true" ]]; then
     args+=(--skip-versioning)
+  fi
+  if [[ "${usage_skip_upload_spec:-}" == "true" ]]; then
+    args+=(--skip-upload-spec)
   fi
   speakeasy run "${args[@]}"
 }
