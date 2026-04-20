e57c0e94dce75b72599761707eb5f05a5b3c5d3e
tgmendes <tiago@speakeasy.com>
2026-04-20 12:11:57 +0100

    chore: remove dd_insights.sh from repo

--- dd_insights.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-export PATH="$HOME/.local/bin:/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:$PATH"
-
-cd "$HOME/github.com/speakeasy-api/gram"
-
-"$HOME/.local/bin/claude" --print "Run the datadog-insights skill"
