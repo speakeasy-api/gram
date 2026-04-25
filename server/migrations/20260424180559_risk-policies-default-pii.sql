cf686df8e1a70bdcc16d35242876015d3231bd84
David Alberto Adler <dalberto.adler@gmail.com>
2026-04-25 11:01:59 +0100

    fix: include all Presidio entities in default backfill

--- server/migrations/20260424180559_risk-policies-default-pii.sql
+++ server/migrations/20260424180559_risk-policies-default-pii.sql
@@ -1,7 +1,7 @@
 -- Enable PII scanning on all existing risk policies that only have gitleaks.
 UPDATE risk_policies
 SET sources = sources || '{presidio}'
-  , presidio_entities = '{PERSON,EMAIL_ADDRESS,PHONE_NUMBER,LOCATION,IP_ADDRESS,URL,MAC_ADDRESS,DATE_TIME,NRP}'
+  , presidio_entities = '{PERSON,EMAIL_ADDRESS,PHONE_NUMBER,LOCATION,IP_ADDRESS,URL,MAC_ADDRESS,DATE_TIME,NRP,CREDIT_CARD,CRYPTO,IBAN_CODE,US_BANK_NUMBER,US_SSN,US_ITIN,US_PASSPORT,US_DRIVER_LICENSE,MEDICAL_LICENSE,UK_NHS}'
   , version = version + 1
 WHERE NOT sources @> '{presidio}'
   AND deleted IS FALSE;
