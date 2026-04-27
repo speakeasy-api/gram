0d9705507d88c6e202285a9f61cf7d4649b68d29
David Alberto Adler <dalberto.adler@gmail.com>
2026-04-27 12:15:16 -0700

    fix: keep IP_ADDRESS and MAC_ADDRESS, only remove URL/DATE_TIME/NRP

--- server/migrations/20260427000002_remove-noisy-pii-entities.sql
+++ server/migrations/20260427000002_remove-noisy-pii-entities.sql
@@ -1,21 +1,15 @@
 -- Remove non-PII entities that were incorrectly backfilled into the PII
--- category by 20260424180559_risk-policies-default-pii.sql. These cause
--- excessive false positives in a coding context:
---   URL         - every URL in code/docs/configs
---   IP_ADDRESS  - common in configs, logs, networking code
---   MAC_ADDRESS - common in networking code
---   DATE_TIME   - ubiquitous in any codebase
---   NRP         - nationality/religious/political group, not PII
+-- category by 20260424180559_risk-policies-default-pii.sql. These are not
+-- personally identifiable information:
+--   URL       - not PII, every URL in code/docs/configs would trigger
+--   DATE_TIME - not PII, ubiquitous in any codebase
+--   NRP       - nationality/religious/political group, not PII
 UPDATE risk_policies
 SET presidio_entities = array_remove(
       array_remove(
-        array_remove(
-          array_remove(
-            array_remove(presidio_entities, 'URL'),
-          'IP_ADDRESS'),
-        'MAC_ADDRESS'),
+        array_remove(presidio_entities, 'URL'),
       'DATE_TIME'),
     'NRP')
   , version = version + 1
-WHERE presidio_entities && '{URL,IP_ADDRESS,MAC_ADDRESS,DATE_TIME,NRP}'
+WHERE presidio_entities && '{URL,DATE_TIME,NRP}'
   AND deleted IS FALSE;
