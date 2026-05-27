6e04943c531658ab2442bacb83ba447260cd1b23
Vishal Gowda <cartmanboy1991@gmail.com>
2026-05-27 17:53:08 +0100

    Revert "feat(db): absorb pgcli into db:connect via --pretty flag"
    This reverts commit 3d1dc16e35bdb699acf2864e499804681f1b2e05.

--- .mise-tasks/db/connect.sh
+++ .mise-tasks/db/connect.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-#MISE description="Connect to the local development database with psql (or pgcli with --pretty)"
+#MISE description="Connect to the local development database with psql"
 
-#USAGE flag "--pretty" help="Use pgcli instead of psql"
-#USAGE arg "<db_args>..." help="Extra arguments forwarded to psql/pgcli" default=""
+#USAGE arg "<psql_args>..." help="Extra arguments forwarded to psql" default=""
 
 set -eo pipefail
 
-if [ "${usage_pretty:-}" = "true" ]; then
-  exec pgcli "${GRAM_DATABASE_URL%&search_path=*}" ${usage_db_args:-}
-else
-  exec psql "${GRAM_DATABASE_URL%&search_path=*}" ${usage_db_args:-}
-fi
+exec psql "${GRAM_DATABASE_URL%&search_path=*}" ${usage_psql_args:-}
