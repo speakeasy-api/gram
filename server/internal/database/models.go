820330c0b07197c2fcf2745a7568ce418e036c94
dennnis-ez <dennis@speakeasy.com>
2026-05-28 10:47:36 +0100

    mig: fetch unanalyzed query and logic optimizations
    Split out of https://github.com/speakeasy-api/gram/pull/3080.

--- server/internal/database/models.go
+++ server/internal/database/models.go
@@ -267,6 +267,7 @@ type ChatMessage struct {
 	ContentHash      []byte
 	Generation       int32
 	CreatedAt        pgtype.Timestamptz
+	RiskAnalyzedAt   pgtype.Timestamptz
 }
 
 type ChatResolution struct {
