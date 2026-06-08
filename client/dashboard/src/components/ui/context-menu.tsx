32b494cb93b4b67790bac2ea5d1407193766fc83
Sagar Batchu <sagar.batchu92@gmail.com>
2026-06-08 12:21:24 -0700

    fix(dashboard): drop unused ContextMenuSeparator export (knip)

--- client/dashboard/src/components/ui/context-menu.tsx
+++ client/dashboard/src/components/ui/context-menu.tsx
@@ -60,25 +60,4 @@ function ContextMenuItem({
   );
 }
 
-function ContextMenuSeparator({
-  className,
-  ...props
-}: React.ComponentProps<
-  typeof ContextMenuPrimitive.Separator
->): React.JSX.Element {
-  return (
-    <ContextMenuPrimitive.Separator
-      data-slot="context-menu-separator"
-      className={cn("bg-border -mx-1 my-1 h-px", className)}
-      {...props}
-    />
-  );
-}
-
-export {
-  ContextMenu,
-  ContextMenuTrigger,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuSeparator,
-};
+export { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuItem };
