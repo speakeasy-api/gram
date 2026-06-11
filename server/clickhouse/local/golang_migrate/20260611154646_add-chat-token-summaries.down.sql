5ff37be20946cf8a766b4d75d6c97494f0167834
Chase <chasecrumbaugh4@gmail.com>
2026-06-11 08:53:00 -0700

    regen

--- server/clickhouse/local/golang_migrate/20260611154646_add-chat-token-summaries.down.sql
+++
@@ -1,4 +0,0 @@
--- reverse: create "chat_token_summaries_mv" view
-DROP VIEW `chat_token_summaries_mv`;
--- reverse: drop "chat_token_summaries_mv" view
-CREATE MATERIALIZED VIEW `chat_token_summaries_mv` TO `chat_token_summaries` AS SELECT gram_project_id, toString(attributes.gen_ai.conversation.id) AS chat_id, toStartOfDay(fromUnixTimestamp64Nano(time_unix_nano)) AS time_bucket, sumIf(toInt64OrZero(toString(attributes.gen_ai.usage.total_tokens)), toString(attributes.gen_ai.usage.total_tokens) != '') AS total_tokens, toUInt64(countIf(startsWith(gram_urn, 'tools:') OR (toString(attributes.gram.tool.urn) != '') OR (toString(attributes.gram.event.source) != '') OR (toString(attributes.gen_ai.usage.total_tokens) = ''))) AS stored_event_count FROM telemetry_logs WHERE toString(attributes.gen_ai.conversation.id) != '' GROUP BY gram_project_id, chat_id, time_bucket;
