c452a9f36a533724d7b257101680fb6695aea8aa
daniel <daniel@speakeasy.com>
2026-08-12 17:51:03 +0100

    mig: mark LiteLLM-proxied sessions at the chat level
    Split out of https://github.com/speakeasy-api/gram/pull/5187.

--- server/internal/database/models.go
+++ server/internal/database/models.go
@@ -361,6 +361,7 @@ type Chat struct {
 	Summary            pgtype.Text
 	SummaryGeneratedAt pgtype.Timestamptz
 	UserAccountID      uuid.NullUUID
+	LitellmProxied     bool
 	CreatedAt          pgtype.Timestamptz
 	UpdatedAt          pgtype.Timestamptz
 	DeletedAt          pgtype.Timestamptz
