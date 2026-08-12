919beeba38d14fd3712627edb43ec42b70597344
tgmendes <tiago@speakeasy.com>
2026-08-12 11:38:44 +0100

    feat(clickhouse): pre-aggregate challenge buckets

---
+++ server/clickhouse/local/golang_migrate/20260811155533_challenge-bucket-summaries.down.sql
@@ -0,0 +1,4 @@
+-- reverse: create "authz_challenge_bucket_summaries_mv" view
+DROP VIEW `authz_challenge_bucket_summaries_mv`;
+-- reverse: create "authz_challenge_bucket_summaries" table
+DROP TABLE `authz_challenge_bucket_summaries`;
