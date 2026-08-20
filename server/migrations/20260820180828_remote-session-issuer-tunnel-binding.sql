294d3b540d344636e4ed49a34721a2d8a5a6f6d7
Quinn Stearns <quinn@speakeasy.com>
2026-08-20 11:43:58 -0700

    Revert "mig: bind remote session issuers to MCP tunnels"
    This reverts commit 27081a8bc98e240176ebe1b7a9a1c60d6c954300.

--- server/migrations/20260820180828_remote-session-issuer-tunnel-binding.sql
+++
@@ -1,2 +0,0 @@
--- Modify "remote_session_issuers" table
-ALTER TABLE "remote_session_issuers" ADD COLUMN "tunneled_mcp_server_id" uuid NULL, ADD CONSTRAINT "remote_session_issuers_tunneled_mcp_server_id_fkey" FOREIGN KEY ("tunneled_mcp_server_id") REFERENCES "tunneled_mcp_servers" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
