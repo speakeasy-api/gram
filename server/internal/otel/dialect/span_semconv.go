0ce3fd3486ee95c1fa4305fec9db943bcda37b87
Georges Haidar <ghaidar0@gmail.com>
2026-08-20 13:54:41 +0100

    refactor(otel): reuse dialect attribute lookup

--- server/internal/otel/dialect/span_semconv.go
+++ server/internal/otel/dialect/span_semconv.go
@@ -23,57 +23,17 @@ func (e SemconvSpan) Content(span *otelv1.InboundSpan) (string, []string, error)
 }
 
 func (e SemconvSpan) SessionID(span *otelv1.InboundSpan) (key string, val string, err error) {
-	const desired = "gen_ai.conversation.id"
-
-	for _, kv := range span.GetAttributes() {
-		if kv.GetKey() != desired || !kv.GetValue().HasStringValue() {
-			continue
-		}
-
-		return desired, kv.GetValue().GetStringValue(), nil
-	}
-
-	return "", "", nil
+	return getOneAttr(span, "gen_ai.conversation.id")
 }
 
 func (e SemconvSpan) ExternalUserEmail(span *otelv1.InboundSpan) (key string, val string, err error) {
-	const desired = "user.email"
-
-	for _, kv := range span.GetAttributes() {
-		if kv.GetKey() != desired || !kv.GetValue().HasStringValue() {
-			continue
-		}
-
-		return desired, kv.GetValue().GetStringValue(), nil
-	}
-
-	return "", "", nil
+	return getOneAttr(span, "user.email")
 }
 
 func (e SemconvSpan) ExternalUserID(span *otelv1.InboundSpan) (key string, val string, err error) {
-	const desired = "user.id"
-
-	for _, kv := range span.GetAttributes() {
-		if kv.GetKey() != desired || !kv.GetValue().HasStringValue() {
-			continue
-		}
-
-		return desired, kv.GetValue().GetStringValue(), nil
-	}
-
-	return "", "", nil
+	return getOneAttr(span, "user.id")
 }
 
 func (e SemconvSpan) ResponseID(span *otelv1.InboundSpan) (key string, val string, err error) {
-	const desired = "gen_ai.response.id"
-
-	for _, kv := range span.GetAttributes() {
-		if kv.GetKey() != desired || !kv.GetValue().HasStringValue() {
-			continue
-		}
-
-		return desired, kv.GetValue().GetStringValue(), nil
-	}
-
-	return "", "", nil
+	return getOneAttr(span, "gen_ai.response.id")
 }
