76fad00ac21197e58665edf6cd19e6d9d85a099e
Georges Haidar <ghaidar0@gmail.com>
2026-09-04 01:26:26 +0100

    mig: add read-only ClickHouse marts

---
+++ server/clickhouse/local/golang_migrate/20260904182727_create-marts-views.down.sql
@@ -0,0 +1,6 @@
+-- reverse: create "daily_model_usage" view
+DROP VIEW `marts`.`daily_model_usage`;
+-- reverse: create "daily_function_adoption" view
+DROP VIEW `marts`.`daily_function_adoption`;
+-- reverse: create "daily_agent_adoption" view
+DROP VIEW `marts`.`daily_agent_adoption`;
