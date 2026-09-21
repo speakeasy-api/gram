89c163fe4af09cf2654b8b58007a6a69d1080075
aa-wong <waaron.wong@gmail.com>
2026-09-21 14:24:32 -0700

    mig: add the authentication host flag to user session issuers

--- server/internal/database/models.go
+++ server/internal/database/models.go
@@ -3525,6 +3525,7 @@ type UserSessionIssuer struct {
 	ClientIDMetadataAdmissionMode pgtype.Text
 	TrustedRemoteSessionIssuerID  uuid.NullUUID
 	TrustedRemoteSessionClientID  uuid.NullUUID
+	UseAuthenticationHost         bool
 	CreatedAt                     pgtype.Timestamptz
 	UpdatedAt                     pgtype.Timestamptz
 	DeletedAt                     pgtype.Timestamptz
