eb7e8a2aab06630471bb63e580b4884403d84395
Thomas Rooney <thomas@speakeasyapi.dev>
2026-09-23 12:20:55 -0700

    fix: count all registered worker schedules

--- server/internal/background/worker_test.go
+++ server/internal/background/worker_test.go
@@ -106,7 +106,7 @@ func TestWorkers_RegisterSchedulesPreservesManualPauses(t *testing.T) {
 		var err error
 		ids, err = scheduleIDs(ctx, env.Client())
 		assert.NoError(c, err)
-		assert.Len(c, ids, 24, "all unconditional schedules should be registered")
+		assert.Len(c, ids, 25, "all unconditional schedules should be registered")
 	}, 30*time.Second, 250*time.Millisecond)
 	sc := env.Client().ScheduleClient()
 	windows := make(map[string]time.Duration, len(ids))
